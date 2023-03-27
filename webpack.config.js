b4e941f656b4b6d991d078c98bc0439b6877a617
unknown <mohsen.akb1377@gmail.com>
2023-03-27 14:30:50 +0330

    use prettier to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,34 @@
 const path = require('path');
-module.exports ={
-    mode:'development',
-    entry:'./src/index.js',
-    output:{
-       path: path.resolve(__dirname,'dist') ,
-       filename:'bundle.js'
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    devServer:
-    {
-        static:
-     {
-        directory:path.resolve(__dirname,'dist')
-     },
-     port:3000,
-     open:true,
-     hot:true,
-     compress:true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            include: path.resolve(__dirname, 'src'),
-            use: ['style-loader', 'css-loader', 'postcss-loader'],
-          },
-          {
-            test: /\.(png|svg|jpe?g|gif)$/i,
-            loader: 'file-loader',
-            options: {
-              outputPath: './assets/img',
-            },
-            },
-        ],
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
-}
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: './assets/img',
+        },
+      },
+    ],
+  },
+};
